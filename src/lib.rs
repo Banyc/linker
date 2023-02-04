071157c93bc337fb46777907b0734bf32294ec94
Banyc <36535895+Banyc@users.noreply.github.com>
2023-02-04 20:27:25 +0800

    refactor: code reuse

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@ where
     let new_symbol_index = match existing_symbol_index {
         Some(existing_symbol_index) => {
             let existing_symbol = symbol_table.get(existing_symbol_index);
-            let definition = match (existing_symbol.value, symbol.value) {
+            let _ = match (existing_symbol.value, symbol.value) {
                 (SymbolValue::Undefined, SymbolValue::Undefined) => {
                     return Ok(existing_symbol_index)
                 }
@@ -95,38 +95,41 @@ where
             };
 
             // Replace the existing symbol
-            let new_offset = section_table.len(definition.section) + definition.offset;
-            let new_symbol = Symbol {
-                value: SymbolValue::Defined(SymbolDefinition {
-                    offset: new_offset,
-                    ..definition
-                }),
-                ..*symbol
-            };
+            let new_symbol = update_offset(section_table, symbol);
             symbol_table.replace(existing_symbol_index, new_symbol);
             existing_symbol_index
         }
         None => {
             // Add the symbol to symbol table
-            let new_symbol = match symbol.value {
-                SymbolValue::Undefined => *symbol,
-                SymbolValue::Defined(definition) => {
-                    let new_offset = section_table.len(definition.section) + definition.offset;
-                    Symbol {
-                        value: SymbolValue::Defined(SymbolDefinition {
-                            offset: new_offset,
-                            ..definition
-                        }),
-                        ..*symbol
-                    }
-                }
-            };
+            let new_symbol = update_offset(section_table, symbol);
             let new_symbol_index = symbol_table.add(new_symbol);
             new_symbol_index
         }
     };
 
     return Ok(new_symbol_index);
+}
+
+fn update_offset<'name, S>(
+    section_table: &mut impl LoadableSectionTable,
+    symbol: &Symbol<'name, S>,
+) -> Symbol<'name, S>
+where
+    S: SectionIndex,
+{
+    match symbol.value {
+        SymbolValue::Undefined => *symbol,
+        SymbolValue::Defined(definition) => {
+            let new_offset = section_table.len(definition.section) + definition.offset;
+            Symbol {
+                value: SymbolValue::Defined(SymbolDefinition {
+                    offset: new_offset,
+                    ..definition
+                }),
+                ..*symbol
+            }
+        }
+    }
 }
 
 /// # Panic
